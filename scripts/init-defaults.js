8baf584f611965a077d992ee0294101f699e44c0
Ivan Belikov <wanjas@gmail.com>
2020-11-12 19:26:11 +0900

    Postinstall default init

--- scripts/init-defaults.js
+++ scripts/init-defaults.js
@@ -1,32 +1,45 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
+function searchForProjectRoot(moduleRoot) {
+  const parts = moduleRoot.split('/node_modules');
+
+  // we are sub-dependency or we have weird path, so we should NOT copy anything
+  // normally moduleRoot is something like
+  // /home/user/my_project/node_modules/@wanjas/dev
+  if (parts.length != 2) {
+    return null;
+  }
+
+  return parts[0];
+}
 
 function main() {
-  const moduleRoot = path.resolve(__dirname, "..");
-  const projectRoot = process.cwd();
-
-  console.log(`Filename: ${require.main.path}`);
+  const moduleRoot = path.resolve(__dirname, '..');
+  const projectRoot = searchForProjectRoot(moduleRoot);
 
   console.log(`Module root: ${moduleRoot}`);
   console.log(`Project root: ${projectRoot}`);
 
-  const defaultFiles = [".eslintrc.js", ".prettierrc.js", "tsconfig.json"];
+  if (projectRoot) {
+    const defaultFiles = ['.eslintrc.js', '.prettierrc.js', 'tsconfig.json'];
 
-  defaultFiles.forEach((filename) => {
-    const source = path.resolve(moduleRoot, "_default_files", filename);
-    const destination = path.resolve(projectRoot, filename);
+    defaultFiles.forEach((filename) => {
+      const source = path.resolve(moduleRoot, '_default_files', filename);
+      const destination = path.resolve(projectRoot, filename);
 
-    if (!fs.existsSync(destination)) {
-      if (!fs.existsSync(source)) {
-        throw new Error(`There is no predefined file [${filename}]`);
+      if (!fs.existsSync(destination)) {
+        if (!fs.existsSync(source)) {
+          throw new Error(`There is no predefined file [${filename}]`);
+        }
+
+        console.log(`Created default [${filename}] in project`);
+        fs.copyFileSync(source, destination);
+      } else {
+        console.log(`File [${filename}] exists in project`);
       }
-
-      console.log(`Created default [${filename}] in project`);
-      // fs.copyFileSync(source, destination);
-    } else {
-      console.log(`File [${filename}] exists in project`);
-    }
-  });
+    });
+  }
   process.exit(0);
 }
 
