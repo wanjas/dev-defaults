aa314e43f5643909ea04797f0ef7d34023016338
Ivan Belikov <wanjas@gmail.com>
2020-11-12 19:45:45 +0900

    Deps update; .gitignore

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
+    project: 'tsconfig.json',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint/eslint-plugin"],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
   root: true,
   env: {
@@ -20,33 +20,33 @@ module.exports = {
     jest: true,
   },
   rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-use-before-define": "off", // not working properly
-    "no-param-reassign": "off",
-    "max-classes-per-file": "off",
-    "import/prefer-default-export": "off",
-    "lines-between-class-members": [
-      "error",
-      "always",
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off', // not working properly
+    'no-param-reassign': 'off',
+    'max-classes-per-file': 'off',
+    'import/prefer-default-export': 'off',
+    'lines-between-class-members': [
+      'error',
+      'always',
       { exceptAfterSingleLine: true },
     ],
 
     // I've failed to make this rule to work properly
-    "import/extensions": [
-      "off",
-      "never",
+    'import/extensions': [
+      'off',
+      'never',
       {
-        ts: "never",
-        tsx: "never",
-        js: "never",
-        jsx: "never",
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
       },
     ],
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-    "class-methods-use-this": "warn",
-    "react/prop-types": "off",
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'class-methods-use-this': 'warn',
+    'react/prop-types': 'off',
   },
 };
