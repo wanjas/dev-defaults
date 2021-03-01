6a1e2774842a220f1650fdfb8d2572783fa179ad
Ivan Belikov <wanjas@gmail.com>
2021-03-01 13:00:14 +0900

    prefer-arrow-callback: off

--- eslint/react/.eslintrc.js
+++ eslint/react/.eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     'react/prop-types': 'off',
+    'prefer-arrow-callback': "off", // Doesn't make sense in react applications with for example React.memo
   },
 };
